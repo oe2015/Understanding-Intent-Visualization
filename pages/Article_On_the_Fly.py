ecca43fd8a46b9cb9837e27d958980a12c483f67
Alaa24ahmed <alaaahmed422002@gmail.com>
2024-09-12 16:59:06 +0400

    article_on_thefly

--- pages/Article_On_the_Fly.py
+++ pages/Article_On_the_Fly.py
@@ -144,74 +144,159 @@ with c30:
 #         """
 #     )
 #     st.markdown("")
+framing_explanations = {
+"Economic": "Costs, benefits, or other financial implications",
+"Capacity and resources": "Availability of physical, human or financial resources, and capacity of current systems",
+"Morality": "Religious or ethical implications",
+"Fairness and equality": "Balance or distribution of rights, responsibilities, and resources",
+"Legality Constitutionality and jurisprudence": "Rights, freedoms, and authority of individuals, corporations, and government",
+"Policy prescription and evaluation": "Discussion of specific policies aimed at addressing problems",
+"Crime and punishment": "Effectiveness and implications of laws and their enforcement",
+"Security and defense": "Threats to welfare of the individual, community, or nation",
+"Health and safety": "Health care, sanitation, public safety",
+"Quality of life": "Threats and opportunities for the individual's wealth, happiness, and well-being",
+"Cultural identity": "Traditions, customs, or values of a social group in relation to a policy issue",
+"Public opinion": "Attitudes and opinions of the general public, including polling and demographics",
+"Political": "Considerations related to politics and politicians, including lobbying, elections, and attempts to sway voters",
+"External regulation and reputation": "International reputation or foreign policy"
+}
+
+techniques = {
+    "Name Calling or Labelling": "Using insulting or desirable labels for individuals or groups.",
+    "Guilt by Association": "Attacking by linking to negatively viewed groups or concepts.",
+    "Casting Doubt": "Undermining credibility by questioning character.",
+    "Appeal to Hypocrisy": "Accusing of hypocrisy to attack reputation.",
+    "Questioning the Reputation": "Undermining character with negative claims.",
+    "Flag Waiving": "Justifying ideas by appealing to group pride or benefits.",
+    "Appeal to Authority": "Citing authority to support an argument.",
+    "Appeal to Popularity": "Claiming widespread agreement to justify a stance.",
+    "Appeal to Values": "Linking ideas to positive values.",
+    "Appeal to Fear, Prejudice": "Using fear or prejudice to promote or reject ideas.",
+    "Strawman": "Misrepresenting an argument to refute it easily.",
+    "Red Herring": "Distracting from the main issue with irrelevant topics.",
+    "Whataboutism": "Accusing of hypocrisy without disproving the argument.",
+    "Causal Oversimplification": "Oversimplifying causes of an issue.",
+    "False Dilemma or No Choice": "Presenting only two options when more exist.",
+    "Consequential Oversimplification": "Claiming improbable chain reactions.",
+    "Slogans": "Using catchy phrases with emotional appeal.",
+    "Conversation Killer": "Discouraging discussion with dismissive phrases.",
+    "Appeal to Time": "Arguing that it's the right time for action.",
+    "Loaded Language": "Using emotionally charged words to influence.",
+    "Obfuscation, Intentional Vagueness, Confusion": "Being unclear to allow varied interpretations.",
+    "Exaggeration or Minimisation": "Overstating or downplaying significance.",
+    "Repetition": "Repeating phrases to persuade."
+}
 with st.expander("About FRAPPE", expanded=False):
-    st.markdown(
-        """
-        ## Welcome to FRAPPE: Your FRAming, Persuasion, and Propaganda Explorer
+    st.markdown("""
+    ## Welcome to FRAPPE: Your FRAming, Persuasion, and Propaganda Explorer
 
-        In today's information-rich world, distinguishing reliable news from misleading content is more crucial than ever. FRAPPE is here to help you navigate this complex landscape.
+    In today's information-rich world, distinguishing reliable news from misleading content is more crucial than ever. FRAPPE is here to help you navigate this complex landscape.
 
-       ### What FRAPPE Does:
-        - Analyzes news articles beyond the surface level
-        - Uncovers linguistic techniques that shape opinions and emotions
-        - Analyzes articles for specific frames and persuasion techniques, including:
-          - Frames:
-            - Economic
-            - Capacity and resources
-            - Morality
-            - Fairness and equality
-            - Legality Constitutionality and jurisprudence
-            - Policy prescription and evaluation
-            - Crime and punishment
-            - Security and defense
-            - Health and safety
-            - Quality of life
-            - Cultural identity
-            - Public opinion
-            - Political
-            - External regulation and reputation
-          - Persuasion Techniques:
-            - Name Calling or Labelling
-            - Guilt by Association
-            - Casting Doubt
-            - Appeal to Hypocrisy
-            - Questioning the Reputation
-            - Flag Waiving
-            - Appeal to Authority
-            - Appeal to Popularity
-            - Appeal to Values
-            - Appeal to Fear, Prejudice
-            - Strawman
-            - Red Herring
-            - Whataboutism
-            - Causal Oversimplification
-            - False Dilemma or No Choice
-            - Consequential Oversimplification
-            - Slogans
-            - Conversation Killer
-            - Appeal to Time
-            - Loaded Language
-            - Obfuscation, Intentional Vagueness, Confusion
-            - Exaggeration or Minimisation
-            - Repetition
-        - Compares persuasion and framing strategies across:
-          - Various news outlets
-          - Multiple countries
-          - Different languages and topics
-          
-        ### Why Use FRAPPE:
-        - Gain deeper insights into news content
-        - Understand how information is presented and potentially manipulated
-        - Develop a more critical and informed approach to media consumption
+    ### What FRAPPE Does:
+    - Analyzes news articles beyond the surface level
+    - Uncovers linguistic techniques that shape opinions and emotions
+    - Analyzes articles for specific frames and persuasion techniques, including:
+    """)
 
-        ### Learn More:
-        - Read our research paper: [FRAPPE: FRAming, Persuasion, and Propaganda Explorer](https://aclanthology.org/2024.eacl-demo.22/)
-        - Watch our demo video: [FRAPPE in Action](https://aclanthology.org/2024.eacl-demo.22.mp4)
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.markdown("#### Frames:")
+        for frame in framing_explanations.keys():
+            st.markdown(f"- {frame}", help=framing_explanations[frame])
 
-        Empower yourself with FRAPPE and become a more discerning news reader!
-        """
-    )
-    st.markdown("")
+    with col2:
+        st.markdown("#### Persuasion Techniques:")
+        for technique in techniques.keys():
+            st.markdown(f"- {technique}", help=techniques[technique])
+
+    st.markdown("""
+    - Compares persuasion and framing strategies across:
+    - Various news outlets
+    - Multiple countries
+    - Different languages and topics
+
+    ### Why Use FRAPPE:
+    - Gain deeper insights into news content
+    - Understand how information is presented and potentially manipulated
+    - Develop a more critical and informed approach to media consumption
+
+    ### Learn More:
+    - Read our research paper: [FRAPPE: FRAming, Persuasion, and Propaganda Explorer](https://aclanthology.org/2024.eacl-demo.22/)
+    - Watch our demo video: [FRAPPE in Action](https://aclanthology.org/2024.eacl-demo.22.mp4)
+
+    Empower yourself with FRAPPE and become a more discerning news reader!
+    """)
+
+st.markdown("")
+st.markdown("## 🚀 On-The-Fly Analysis!")
+    # st.markdown(
+    #     """
+    #     ## Welcome to FRAPPE: Your FRAming, Persuasion, and Propaganda Explorer
+
+    #     In today's information-rich world, distinguishing reliable news from misleading content is more crucial than ever. FRAPPE is here to help you navigate this complex landscape.
+
+    # ### What FRAPPE Does:
+    #     - Analyzes news articles beyond the surface level
+    #     - Uncovers linguistic techniques that shape opinions and emotions
+    #     - Analyzes articles for specific frames and persuasion techniques, including:
+    #     - Frames:
+    #         - Economic
+    #         - Capacity and resources
+    #         - Morality
+    #         - Fairness and equality
+    #         - Legality Constitutionality and jurisprudence
+    #         - Policy prescription and evaluation
+    #         - Crime and punishment
+    #         - Security and defense
+    #         - Health and safety
+    #         - Quality of life
+    #         - Cultural identity
+    #         - Public opinion
+    #         - Political
+    #         - External regulation and reputation
+    #     - Persuasion Techniques:
+    #         - Name Calling or Labelling
+    #         - Guilt by Association
+    #         - Casting Doubt
+    #         - Appeal to Hypocrisy
+    #         - Questioning the Reputation
+    #         - Flag Waiving
+    #         - Appeal to Authority
+    #         - Appeal to Popularity
+    #         - Appeal to Values
+    #         - Appeal to Fear, Prejudice
+    #         - Strawman
+    #         - Red Herring
+    #         - Whataboutism
+    #         - Causal Oversimplification
+    #         - False Dilemma or No Choice
+    #         - Consequential Oversimplification
+    #         - Slogans
+    #         - Conversation Killer
+    #         - Appeal to Time
+    #         - Loaded Language
+    #         - Obfuscation, Intentional Vagueness, Confusion
+    #         - Exaggeration or Minimisation
+    #         - Repetition
+    #     - Compares persuasion and framing strategies across:
+    #     - Various news outlets
+    #     - Multiple countries
+    #     - Different languages and topics
+        
+    #     ### Why Use FRAPPE:
+    #     - Gain deeper insights into news content
+    #     - Understand how information is presented and potentially manipulated
+    #     - Develop a more critical and informed approach to media consumption
+
+    #     ### Learn More:
+    #     - Read our research paper: [FRAPPE: FRAming, Persuasion, and Propaganda Explorer](https://aclanthology.org/2024.eacl-demo.22/)
+    #     - Watch our demo video: [FRAPPE in Action](https://aclanthology.org/2024.eacl-demo.22.mp4)
+
+    #     Empower yourself with FRAPPE and become a more discerning news reader!
+    #     """
+    # )
+    # st.markdown("")
 st.markdown("")
 st.markdown("## 🚀 On-The-Fly Analysis!")
 
