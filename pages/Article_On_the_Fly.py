087db62f045f615aaf91af4f6765d560e9662afc
oe2015 <84914454+oe2015@users.noreply.github.com>
2024-10-05 11:41:18 +0400

    Update Article_On_the_Fly.py

--- pages/Article_On_the_Fly.py
+++ pages/Article_On_the_Fly.py
@@ -357,8 +357,8 @@ if text:
     
         chart = alt.Chart(sorted_df).mark_bar().encode(
             x='Probabilities',
-            y=alt.Y('Labels', sort='-x', title='Labels', axis=alt.Axis(labelLimit=200)),  # Wrap long labels
-            color=alt.Color('Labels', scale=alt.Scale(scheme='category10'), legend=alt.Legend(labelLimit=100)),
+            y=alt.Y('Labels', sort='-x', title='Labels', axis=alt.Axis(labelLimit=300)),  # Wrap long labels
+            color=alt.Color('Labels', scale=alt.Scale(scheme='category10'), legend=alt.Legend(labelLimit=300)),
             tooltip=['Probabilities', 'Labels']
         ).properties(
             width=1000,
